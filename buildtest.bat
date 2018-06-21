7e35cafa4f7c12a73cdb6b130cc7fd3d14c3f3bc
prospero78su <prospero.78.su@gmail.com>
2018-06-21 14:33:16 +0200

    038 Модификация сборки теста

--- buildtest.bat
+++ buildtest.bat
@@ -1,7 +1,7 @@
 echo off
 cd test
-..\build\Poc /B Buildfile /sym ..\source
+..\bin\oc /B Buildfile /sym ..\source
 cd ..
 echo on
-copy build\Patchouli.BigNums.dll test\
-copy build\Oberon07.*.dll test\
+copy bin\Patchouli.BigNums.dll test\
+copy bin\Oberon07.*.dll test\

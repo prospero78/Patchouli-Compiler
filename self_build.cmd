0b2fd51855da53bceb3cd98a4b49e2cfe4686a8c
prospero78su <prospero.78.su@gmail.com>
2018-06-21 12:27:31 +0200

    035 Обновление файла сборки

--- self_build.cmd
+++ self_build.cmd
@@ -5,13 +5,13 @@ rem 0. Перейти в каталог с исходниками
 cd source
 echo _
 echo ================= 1. Скомпилировать список файлов ===================
-..\bin\Poc /B Buildfile
+..\bin\oc /B Buildfile
 echo _
 echo ================= 2. Скопировать на место существующих ================
 copy .\*.exe ..\bin
 copy .\*dll ..\bin
 echo _
 echo ================ 3. Ещё раз скомпилировать ========================
-..\bin\Poc /B Buildfile
+..\bin\oc /B Buildfile
 cd ..
 echo on
